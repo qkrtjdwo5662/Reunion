e14ddc83c604a3e7ab6f3f7274a608b6fc11535e
qkrtjdwo5662 <qkrtjdwo5662@naver.com>
2022-10-12 16:44:16 +0900

    setting databases
    crud function setting..

--- src/databases/UserDAO.java
+++ src/databases/UserDAO.java
@@ -3,15 +3,45 @@ package databases;
 import java.sql.*;
 
 public class UserDAO {
+    Connection connection = null;
     PreparedStatement ps = null;
+
+    ResultSet rs = null;
+    Statement st = null;
     public UserDAO(){
 
     }
+    public void databasesClose() {
+        try {
+            if (rs != null)
+                rs.close();
+            if (st != null)
+                st.close();
+            if (ps != null)
+                ps.close();
+        } catch (Exception e) {
+            System.out.println(e + ": dbClose fail");
+        }
+    }
     //CRUD
-    public void create(){
+    public void create(UserDTO userDTO){
+        try{
+            String sql = "INSERT INTO user(stu_id, password, name) values(?, ?, ?)";
+            ps = connection.prepareStatement(sql);
+            ps.setString(1,userDTO.getId());
+            ps.setString(2,userDTO.getPassword());
+            ps.setString(3,userDTO.getName());
+            ps.executeUpdate();
+        }catch(SQLException sqle){
+            sqle.printStackTrace();
+        }finally {
+            databasesClose();
+        }
 
     }
-    public void read(){
+    public void read(UserDTO userDTO){
+        String id = userDTO.getId();
+        String sql = "SELECT password FROM user WHERE id='" + id + "'";
 
     }
     public void update() {
