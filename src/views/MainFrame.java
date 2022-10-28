71a7c4bf6bf7f35b72aadd0572c2f5a74045bf7b
qkrtjdwo5662 <qkrtjdwo5662@naver.com>
2022-10-28 21:20:34 +0900

    메뉴바 제작 시도

--- src/views/MainFrame.java
+++ src/views/MainFrame.java
@@ -3,10 +3,11 @@ package views;
 import databases.UserVO;
 
 import javax.swing.*;
-import javax.swing.border.EmptyBorder;
+import java.awt.*;
 
 public class MainFrame extends JFrame {
-    private JPanel panel;
+    private JPanel welcomePanel;
+    private JPanel menuPanel;
 
     public MainFrame(UserVO vo){
 
@@ -14,29 +15,40 @@ public class MainFrame extends JFrame {
 
         setLocationRelativeTo(null);// window center
 
-        panel = new JPanel();
-        panel.setBorder(new EmptyBorder(5, 5, 5, 5));
-        setContentPane(panel);
-        panel.setLayout(null);
-
+        welcomePanel = new JPanel();
+        add(welcomePanel);
+        welcomePanel.setLayout(null);
 
         JLabel welcomeLabel = new JLabel( vo.getName()+"님 환영합니다");
         welcomeLabel.setBounds(100, 100, 300, 100);
         welcomeLabel.setHorizontalAlignment(JLabel.CENTER);
-        panel.add(welcomeLabel);
+        welcomePanel.add(welcomeLabel);
 
         JLabel userIdLabel = new JLabel( "ID : " + vo.getId());
         userIdLabel.setBounds(100, 200, 300, 100);
         userIdLabel.setHorizontalAlignment(JLabel.CENTER);
-        panel.add(userIdLabel);
+        welcomePanel.add(userIdLabel);
 
         JLabel passwordLabel = new JLabel( "PASSWORD : " + vo.getPassword());
         passwordLabel.setBounds(100, 300, 300, 100);
         passwordLabel.setHorizontalAlignment(JLabel.CENTER);
-        panel.add(passwordLabel);
+        welcomePanel.add(passwordLabel);
+
+        JLabel menuLabel= new JLabel("Menu");
+        menuLabel.setBounds(0,0,80,500);
+        menuLabel.setHorizontalAlignment(JLabel.CENTER);
+        menuLabel.setOpaque(true);
+        menuLabel.setBackground(Color.white);
+        welcomePanel.add(menuLabel);
+
 
         setSize(500, 500);
         setVisible(true);
 
+    }
+
+    public static void main(String[] args) {
+        UserVO vo = new UserVO();
+        MainFrame mainFrame = new MainFrame(vo);
     }
 }
